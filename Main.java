662078a78db93b628f6c77e5a736dbd4f21a27bb
cletizio <103384285+cletizio@users.noreply.github.com>
2023-08-18 14:33:20 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Declare the variables
+		String firstName = "John";
+		String lastName = "Smith";
+		String streetAddress = "1123 Main St.";
+		String city = "Detroit";
+		String zipCode = "48123";
+		
+		// Print the individuals information to the screen.
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(streetAddress);
+		System.out.println(city);
+		System.out.println(zipCode);
+		
+	}
+}
